778c0c8acf5ba75997e9c40fdcd90fdece676013
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-06-25 00:45:05 +0200

    create const Var for Nav

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const sections = document.querySelectorAll('.section'); //quersSelectorAll wählt alle .section an
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
