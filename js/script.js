22bd48e88218d0f15d3099b5cb95ee0e68bff510
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-07-21 17:37:15 +0200

    buggy textfield

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-/*
-popNone
-popShow
-popOverflow
-*/
-
 
 function emailSubmit() {
     const popup = document.getElementById('popUp');
     popup.classList.remove('popNone');
     popup.classList.add('popShow');
+    clearValues();
+}
+
+function clearValues(){
     document.getElementById('contactEmail').value = '';
     document.getElementById('contactName').value = '';
     document.getElementById('contactSubject').value = '';
-    document.getElementById('contactText').value = '';
+    clearTextArea();
+}
+
+function clearTextArea() {
+    const textArea = document.getElementById('contactText');
+    textArea.value = '';
 }
 
 function closeEmailPopup() {
