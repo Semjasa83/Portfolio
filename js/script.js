e4313eab9bc5a613921f9e96f793a7ce9cac98eb
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-07-21 17:17:19 +0200

    fixed message

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ function emailSubmit() {
     const popup = document.getElementById('popUp');
     popup.classList.remove('popNone');
     popup.classList.add('popShow');
+    document.getElementById('contactEmail').value = '';
+    document.getElementById('contactName').value = '';
+    document.getElementById('contactSubject').value = '';
+    document.getElementById('contactText').value = '';
 }
 
 function closeEmailPopup() {
